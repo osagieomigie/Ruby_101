629fceda708e39358da55905f35ae00578e210c9
Osagie Omigie <osagieomigie@gmail.com>
2021-01-06 23:09:40 -0700

    Updated show action of the User controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,22 @@
 class UsersController < ApplicationController
     include Response #add response module 
 
-    # GET response, used for retrieving all Users from the DB 
+    # GET request, used for retrieving all Users from the DB 
     def index 
         @users = User.all 
         json_response(@users)
     end 
 
+    # GET request, used for retrieving a specific User 
     def show 
+        # retrieve user data from request (params)
+        first_name = params[:first_name]
+        last_name = params[:last_name]
+        email = params[:email]
+
+        # query DB for the retieved data
+        @user = User.where({:first_name => first_name, :last_name => last_name, :email => email})
+        json_response(@user)
     end 
 
     def create 
