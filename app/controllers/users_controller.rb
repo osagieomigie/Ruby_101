4531cac9bc827d29893985c61207b4fe8f22c3ae
Osagie Omigie <osagieomigie@gmail.com>
2021-01-07 21:17:35 -0700

    Added the destroy action for the User controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -34,16 +34,25 @@ class UsersController < ApplicationController
         # query DB for the specific user 
         user = User.where(:email => email)
 
-        @user = user.update(user_params)
-        json_response(@user) # return created user, to signify that the user was created
+        @user = user.update(user_params) #update the user 
+        json_response(@user) # return created user, to signify that the user was updated
     end 
 
+    #DELETE request, to delete a user 
     def destroy 
+        # retrieve user data from request (params)
+        email = params[:email]
+
+        # query DB for the specific user 
+        user = User.find_by(:email => email)
+
+        @user = user.destroy #delete the user 
+        head :no_content # response sent back to client 
     end 
 
     private 
         # whitelist the :first_name, :last_name, :email, :notes variables for mass assignment
-        def user_params 
+        def user_params
             params.permit(:first_name, :last_name, :email, :notes)
-        end 
+        end
 end
