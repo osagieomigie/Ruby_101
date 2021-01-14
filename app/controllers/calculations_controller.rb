02c97ca2b3d9506b9cee90919c2bc45496045964
Osagie Omigie <osagieomigie@gmail.com>
2021-01-13 21:41:50 -0700

    Added show action for calculator controller

--- app/controllers/calculations_controller.rb
+++ app/controllers/calculations_controller.rb
@@ -1,12 +1,17 @@
 class CalculationsController < ApplicationController
     include Response 
 
+    # display all calculations made 
     def index 
         @calculation = Calculation.all # retrieve all calculations from calculations table 
         json_response(@calculation) # respond with the array of calculations
     end 
 
+    # display a specific calculation, associated with a user 
     def show 
+        user_id = params[:user_id] # get user id from request 
+        @calculation = Calculation.find_by(:user_id => user_id)
+        json_response(@calculation) # respond with the users calculations
     end 
 
     def create 
